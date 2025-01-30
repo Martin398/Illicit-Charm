3b21d37adb1e61f2a085ea081adb2595732d92cb
Martin <130831985+Martin398@users.noreply.github.com>
2025-01-30 14:51:42 +0100

    camera collider init 01
    doesn't seem to work much at all. Collider doesn't seem to catch anything at all at least not the player

---
+++ Assets/Styx/AreaManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaManager : MonoBehaviour
+{
+    // cheap script taken from: https://www.youtube.com/watch?v=yaQlRvHgIvE
+
+    public GameObject virtualCamera;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player") && !other.isTrigger)
+        {
+            virtualCamera.SetActive(true);
+            Debug.Log("what");
+        }
+
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("what");
+        }
+    }    
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.CompareTag("Player") && !other.isTrigger)
+        {
+            virtualCamera.SetActive(false);
+        }
+    }
+
+}
